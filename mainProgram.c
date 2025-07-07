3668021c61bc634a55eaf62b00c09930c9cc85ca
BFeng11 <bradyfeng11@gmail.com>
2025-07-07 19:48:57 -0400

    Create mainProgram.c

---
+++ mainProgram.c
@@ -0,0 +1,17 @@
+//HANSEN doing the function that moves the money up from the holder
+
+//move robot given distance
+
+//pickup bill
+
+//release bill
+
+//count bills
+
+//hard stop and reset
+task main()
+{
+
+
+
+}
