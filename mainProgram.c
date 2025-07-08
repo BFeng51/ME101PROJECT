b1b40baac6c47e3d9e91d428657ac93caf4da13b
BFeng11 <bradyfeng11@gmail.com>
2025-07-07 20:32:53 -0400

    Update mainProgram.c

--- mainProgram.c
+++ mainProgram.c
@@ -1,7 +1,24 @@
 //HANSEN doing the function that moves the money up from the holder
 
 //move robot given distance
+void driveRobot(int dist, int speed)
+{
+	motor[motorA]=speed;
+	while(sensorValue[S3]>dist)
+	{}
+	motor[motorA]=0;
 
+
+
+}
+//reset robot to origin (will need to be used in simutaneously with driveRobot)
+void resetRobot(int speed)
+{
+	motor[motorA]=-speed;
+	while(sensorValue[S2]!=1)
+	{}
+	motor[motorA]=0;
+}
 //pickup bill
 
 //release bill
