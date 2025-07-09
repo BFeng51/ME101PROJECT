6aa780472640a55e4c4216f1aff9f07564e304ec
BFeng11 <bradyfeng11@gmail.com>
2025-07-09 18:24:40 -0400

    Update mainProgram.c
    july 9th update pt2.

--- mainProgram.c
+++ mainProgram.c
@@ -1,3 +1,6 @@
+//GLOBAL VARIABLES HERE:
+int billColour = 0;
+
 //HANSEN doing the function that moves the money up from the holder
 
 //move robot given distance
@@ -11,6 +14,7 @@ void driveRobot(int dist, int speed)
 
 
 }
+
 //reset robot to origin (will need to be used in simutaneously with driveRobot)
 void resetRobot(int speed)
 {
@@ -19,16 +23,53 @@ void resetRobot(int speed)
 	{}
 	motor[motorA]=0;
 }
+
 //pickup bill
 
 //release bill
 
-//count bills
+//count bill quantity
+int getNumBills()
 
-//hard stop and reset
-task main()
+//Read Bill Value
+void readBillValue(billColor)
 {
 
+}
+
+//convert color integer to dist in cm
+double getDist()
+
+//hard stop and reset
+void stopEverything()
+{
+	motor[motorA]=-100;
+	while(nMotorEncoder[motorA]>0)
+	{}
+	motor[motorA]=0;
+}
+
+task main()
+{
+bool billsLeft=true;
+
+while(billsLeft&&SensorValue[/*touchsensor*/]!=1)
+{
+readBillValue(billColor);
+
+//PICKUP BILL
+
+nMotorEncoder[motorA]=0;
+driveRobot(getDistance(billColor), 50)
+
+//RELEASE BILL
+
+resetRobot(-50);
+
+//IF NO BILLS LEFT, GET OUT OF THE WHILE LOOP
+
+}
+stopEverything();
 
 
 }
